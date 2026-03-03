b99dc0a6947a844e3b01dcc9a0c8528a8e98c303
Aadit Antony <aaditantony1112@gmail.com>
2026-03-03 11:09:01 +0530

    the connection code of the admin section is done

--- lib/admin/admin_dashboard.dart
+++ lib/admin/admin_dashboard.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:gear_up/admin/pages/approve_admins_pages.dart';
+import 'package:gear_up/admin/pages/service_categories_page.dart';
+import 'package:gear_up/admin/pages/view_bookings_page.dart';
 
 import 'widgets/admin_sidebar.dart';
 import 'pages/approve_service_centers_page.dart';
@@ -39,16 +41,20 @@ class _AdminDashboardState extends State<AdminDashboard> {
   }
 
   Widget getSelectedPage() {
-    switch (selectedIndex) {
-      case 0:
-        return const AdminHomePage();
-      case 1:
-        return const ApproveAdminsPage();
-      case 2:
-        return const ApproveServiceCentersPage();
-      default:
-        return const AdminHomePage();
-    }
+   switch (selectedIndex) {
+  case 0:
+    return Scaffold(body: Center(child: Text("overview"),),);
+  case 1:
+    return  ApproveAdminsPage();
+  case 2:
+    return  ApproveServiceCentersPage();
+  case 3:
+    return  ServiceCategoriesPage();
+  case 4:
+    return  ViewBookingsPage();
+  default:
+    return Scaffold(body: Center(child: Text("overview"),),);
+}
   }
 
   @override
