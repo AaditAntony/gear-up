5703f7ed8883b39a37d376d9e9d9aba44ee63dd6
Aadit Antony <aaditantony1112@gmail.com>
2026-03-03 10:41:04 +0530

    the code of the approved section is done

--- lib/admin/admin_dashboard.dart
+++ lib/admin/admin_dashboard.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:gear_up/admin/pages/approve_admin_pages.dart';
+import 'package:gear_up/admin/pages/approve_admins_pages.dart';
 
 import 'widgets/admin_sidebar.dart';
-import 'pages/admin_home_page.dart';
 import 'pages/approve_service_centers_page.dart';
 
 class AdminDashboard extends StatefulWidget {
