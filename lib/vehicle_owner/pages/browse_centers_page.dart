0ad011f58ca09fff4a0c65be2f6bc1ede9d0104c
Aadit Antony <aaditantony1112@gmail.com>
2026-03-04 09:45:51 +0530

    the code of the center page is done

---
+++ lib/vehicle_owner/pages/browse_centers_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BrowseCentersPage extends StatelessWidget {
+  const BrowseCentersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Browse Service Centers", style: TextStyle(fontSize: 18)),
+      ),
+    );
+  }
+}
