899d8499d2aad50d21ac714fb0bd903671568b61
Aadit Antony <aaditantony1112@gmail.com>
2026-03-05 14:57:17 +0530

    the code of the my vehicle set is done

--- lib/vehicle_owner/pages/owner_dashboard.dart
+++ lib/vehicle_owner/pages/owner_dashboard.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'browse_centers_page.dart';
-import 'my_bookings_page.dart';
-import 'my_vehicles_page.dart';
-import 'profile_page.dart';
+import 'package:gear_up/vehicle_owner/pages/ai_recommendation_page.dart';
+import 'package:gear_up/vehicle_owner/pages/browse_centers_page.dart';
+import 'package:gear_up/vehicle_owner/pages/my_bookings_page.dart';
+import 'package:gear_up/vehicle_owner/pages/my_vehicles_page.dart';
+import 'package:gear_up/vehicle_owner/pages/profile_page.dart';
+
 
 class OwnerDashboard extends StatefulWidget {
   const OwnerDashboard({super.key});
@@ -12,43 +14,65 @@ class OwnerDashboard extends StatefulWidget {
 }
 
 class _OwnerDashboardState extends State<OwnerDashboard> {
+
   int selectedIndex = 0;
 
   final List<Widget> pages = const [
+
+    AIRecommendationPage(),
     BrowseCentersPage(),
     MyBookingsPage(),
     MyVehiclesPage(),
     ProfilePage(),
+
   ];
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       body: pages[selectedIndex],
 
       bottomNavigationBar: BottomNavigationBar(
+
         currentIndex: selectedIndex,
+
         onTap: (index) {
           setState(() {
             selectedIndex = index;
           });
         },
+
         type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        backgroundColor: Colors.white,
 
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.smart_toy),
+            label: "AI",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Centers",
+          ),
+
           BottomNavigationBarItem(
             icon: Icon(Icons.calendar_today),
-            label: "My Bookings",
+            label: "Bookings",
           ),
+
           BottomNavigationBarItem(
             icon: Icon(Icons.directions_car),
-            label: "My Vehicles",
+            label: "Vehicles",
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+
         ],
       ),
     );
